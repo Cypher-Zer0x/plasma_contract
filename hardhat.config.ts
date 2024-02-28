b8897f24138641c72442db83b0736119e9a53a42
Maxime <sowlution.pro@gmail.com>
2024-02-28 14:15:57 -0700

    deploy and submit proofs successfullllll

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.16",
+  defaultNetwork: "mumbai",
+  networks: {
+    mumbai: {
+      url: process.env.MUMBAI_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.MUMBAI_API_KEY || "",
+  },
 };
 
 export default config;
